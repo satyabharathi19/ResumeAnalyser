68a3260c7aaa107296cd27135c5d3c59c94213cf
satyabharathi19 <148929278+satyabharathi19@users.noreply.github.com>
2025-05-14 12:32:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ nltk.download('stopwords')
 import spacy
 from spacy.cli import download
 
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
-
+spacy.load('en_core_web_sm')
 import pandas as pd
 import base64
 import time
